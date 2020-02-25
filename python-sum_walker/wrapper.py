8c543409973da39853a9d23c2e8e1968e24e052e
Shlomi Fish <shlomif@shlomifish.org>
2020-02-25 16:17:01 +0200

    add details

--- python-sum_walker/wrapper.py
+++ python-sum_walker/wrapper.py
@@ -12,9 +12,22 @@ import cookiecutter.main
 def main():
     cookiecutter.main.cookiecutter(
         'gh:Kwpolska/python-project-template',
+        no_input=True,
         overwrite_if_exists=True,
         extra_context={
-            'full_name':'Shlomi Fish',
+            "entry_point": ["none", "cli", "gui", ],
+            "project_name": "Sum walker",
+            "project_short_description":
+                ("Iterate over sums of a certain" +
+                 " number of elements"),
+            "release_date": "2020-02-25",
+            "repo_name": "sum_walker",
+            "version": "0.2.0",
+            "year": "2020",
+            'aur_email': "shlomif@cpan.org",
+            'email': "shlomif@cpan.org",
+            'full_name': 'Shlomi Fish',
+            'github_username': "shlomif",
             },
         )
 
