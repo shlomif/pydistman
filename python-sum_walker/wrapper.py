d0b7abb6f7f81605d61a870c71ec128092d2713a
Shlomi Fish <shlomif@shlomifish.org>
2020-02-25 15:51:23 +0200

    first version

---
+++ python-sum_walker/wrapper.py
@@ -0,0 +1,21 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2020 Shlomi Fish <shlomif@cpan.org>
+#
+# Distributed under terms of the MIT license.
+
+import cookiecutter.main
+
+
+def main():
+    cookiecutter.main.cookiecutter(
+        'gh:Kwpolska/python-project-template',
+        overwrite_if_exists=True,
+        extra_defaults={
+            },
+        )
+
+
+main()
