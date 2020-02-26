dd95cfa2d3684fbb83e4c9ea7acf00ee3c0a4648
Shlomi Fish <shlomif@shlomifish.org>
2020-02-26 18:30:54 +0200

    add an example

---
+++ python-sum_walker/code/examples/taxicab_numbers.py
@@ -0,0 +1,35 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2020 Shlomi Fish <shlomif@cpan.org>
+#
+# Distributed under terms of the MIT license.
+
+from six import print_
+
+from sum_walker import DWIM_SumWalker
+
+
+def main():
+    seq = [0, 1]
+    reached = 2
+
+    def request_more():
+        nonlocal reached
+        nonlocal seq
+        seq.append(reached ** 3)
+        reached += 1
+
+    it = DWIM_SumWalker(2, seq, request_more)
+    while True:
+        sum_, coords = next(it)
+        len_ = len(coords)
+        if len_ > 1:
+            print_("{}\t{}\t{}".format(
+                len_, sum_, " ; ".join(
+                    [" + ".join(["{} ** 3".format(x) for x in c])
+                     for c in coords])))
+
+
+main()
