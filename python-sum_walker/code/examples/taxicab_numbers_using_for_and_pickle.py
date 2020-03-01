771a99ccd7c5a7717a87f7dc77167a9ee1645812
Shlomi Fish <shlomif@shlomifish.org>
2020-03-01 13:31:44 +0200

    trying pickle for persistence - gives an error.

---
+++ python-sum_walker/code/examples/taxicab_numbers_using_for_and_pickle.py
@@ -0,0 +1,56 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2020 Shlomi Fish <shlomif@cpan.org>
+#
+# Distributed under terms of the MIT license.
+#
+# See:
+# https://en.wikipedia.org/wiki/Taxicab_number
+
+import os.path
+import pickle
+
+from six import print_
+
+from sum_walker import DWIM_SumWalker
+
+
+class Seq:
+    def __init__(self):
+        self.seq = [0, 1]
+        self.reached = 2
+
+    def request_more(self):
+        self.seq.append(self.reached ** 3)
+        self.reached += 1
+
+
+def main():
+    fn = 'taxicab.pykl'
+    if os.path.exists(fn):
+        with open(fn, 'rb') as f:
+            [it, seq] = pickle.load(f)
+    else:
+        seq = Seq()
+
+        def request_more():
+            return seq.request_more()
+        it = DWIM_SumWalker(2, seq.seq, request_more)
+    count = init_count = 1000
+    for sum_, coords in it:
+        len_ = len(coords)
+        if len_ > 1:
+            print_("{}\t{}\t{}".format(
+                len_, sum_, " ; ".join(
+                    [" + ".join(["{} ** 3".format(x) for x in c])
+                     for c in coords])))
+        count -= 1
+        if count == 0:
+            count = init_count
+            with open(fn, 'wb') as f:
+                pickle.dump([it, seq], f)
+
+
+main()
