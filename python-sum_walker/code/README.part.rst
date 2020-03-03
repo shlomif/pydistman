3ff61a295eba498a78d0430d4de667021cedecef
Shlomi Fish <shlomif@shlomifish.org>
2020-03-03 14:54:04 +0200

    enhance the READMEs

---
+++ python-sum_walker/code/README.part.rst
@@ -0,0 +1,40 @@
+PURPOSE
+-------
+
+The sum_walker PyPI distribution allows one to iterate over increasing
+sums of a certain number (e.g: 2 or 3) of elements out of a stream of
+increasing integers.
+
+INSTALLATION
+------------
+
+pip3 install sum_walker
+
+USAGE
+-----
+
+    # Finding sums of 2 powers of 3:
+    # https://en.wikipedia.org/wiki/Taxicab_number
+    from sum_walker import DWIM_SumWalker
+
+    seq = [0, 1]
+    reached = 2
+
+    def request_more():
+        nonlocal reached
+        nonlocal seq
+        seq.append(reached ** 3)
+        reached += 1
+
+    it = DWIM_SumWalker(2, seq, request_more)
+    for sum_, coords in it:
+        len_ = len(coords)
+        if len_ > 1:
+            print("{}\t{}\t{}".format(
+                len_, sum_, " ; ".join(
+                    [" + ".join(["{} ** 3".format(x) for x in c])
+                     for c in coords])))
+
+NOTES
+-----
+
--- python-sum_walker/wrapper.py
+++ python-sum_walker/wrapper.py
@@ -22,7 +22,7 @@ def main():
                  " number of elements"),
             "release_date": "2020-02-25",
             "repo_name": "sum_walker",
-            "version": "0.4.0",
+            "version": "0.6.0",
             "year": "2020",
             'aur_email': "shlomif@cpan.org",
             'email': "shlomif@cpan.org",
@@ -36,11 +36,19 @@ def main():
 
     _append("sum_walker/sum_walker/__init__.py", "code/sum_walker/__init__.py")
     chglog = "sum_walker/CHANGELOG.rst"
-    txt = open(chglog, "rt").read()
-    import re
-    txt = re.sub("\n0\\.1\\.0\n.*", "\n" + open(
-        "code/CHANGELOG.rst.base.txt", "rt").read(), txt, re.M | re.S)
-    open(chglog, "wt").write(txt)
+
+    def _re_mutate(fn, pattern, repl_fn, prefix='', suffix=''):
+        txt = open(fn, "rt").read()
+        import re
+        txt, count = re.subn(pattern, prefix + open(
+            repl_fn, "rt").read() + suffix, txt, 1, re.M | re.S)
+        # print(count)
+        assert count == 1
+        open(fn, "wt").write(txt)
+    _re_mutate(chglog, "\n0\\.1\\.0\n.*", "code/CHANGELOG.rst.base.txt", "\n")
+    s = "COPYRIGHT\n"
+    for fn in ["sum_walker/README", "sum_walker/README.rst"]:
+        _re_mutate(fn, "^PURPOSE\n.*?\n" + s, "code/README.part.rst", '', s)
 
     testfn = "sum_walker/tests/test_sum_walker.py"
     _append(testfn,
