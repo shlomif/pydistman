b63f6fe55164cf7b8be83e428fb6753f996f2084
Shlomi Fish <shlomif@shlomifish.org>
2021-08-28 18:10:31 +0300

    release

--- pydistman/python_pypi_dist_manager.py
+++ pydistman/python_pypi_dist_manager.py
@@ -20,7 +20,7 @@ dist_name = "pydistman"
 
 obj = DistManager(
     dist_name=dist_name,
-    dist_version="0.0.4",
+    dist_version="0.0.5",
     project_name="Python Dist Manager",
     project_short_description=(
         "\"Maximum Overkill DRY\" PyPI distribution manager (WiP)"
