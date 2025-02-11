07ffd53d9cece32ca473466d29880367277912b5
Shlomi Fish <shlomif@shlomifish.org>
2025-02-11 09:02:07 +0200

    prepare dist for a release
    New version: 0.0.12

--- pydistman/python_pypi_dist_manager.py
+++ pydistman/python_pypi_dist_manager.py
@@ -15,17 +15,18 @@ dist_name = "pydistman"
 
 obj = DistManager(
     dist_name=dist_name,
-    dist_version="0.0.11",
+    dist_version="0.0.12",
     project_name="Python Dist Manager",
     project_short_description=(
         "\"Maximum Overkill DRY\" PyPI distribution manager (WiP)"
     ),
-    release_date="2024-07-22",
+    release_date="2025-02-11",
     project_year="2020",
     aur_email="shlomif@cpan.org",
     project_email="shlomif@cpan.org",
     full_name="Shlomi Fish",
     github_username="shlomif",
     tox_envlist="py313",
+    entry_point="none",
 )
 obj.cli_run()
